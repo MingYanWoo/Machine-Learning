d6076b7da3f0de07d0b55c84febef101fe2b72ee
MingYanWoo <woomingyan@gmail.com>
2018-04-30 04:30:08 +0800

    handwritingTest

--- kNN/kNN.py
+++ kNN/kNN.py
@@ -1,5 +1,6 @@
 # -*- coding:utf-8 -*-
 from numpy import *
+from os import listdir
 import operator
 
 
@@ -45,37 +46,64 @@ def file2matrix(filename):
 
 # 归一化特征值
 def autoNorm(dataSet):
-	minVals = dataSet.min(0)
-	maxVals = dataSet.max(0)
-	ranges = maxVals - minVals
-	normDataSet = zeros(shape(dataSet))
-	m = dataSet.shape[0]
-	normDataSet = dataSet - tile(minVals, (m,1))
-	normDataSet = normDataSet/tile(ranges, (m,1))
-	return normDataSet, ranges, minVals
+    minVals = dataSet.min(0)
+    maxVals = dataSet.max(0)
+    ranges = maxVals - minVals
+    normDataSet = zeros(shape(dataSet))
+    m = dataSet.shape[0]
+    normDataSet = dataSet - tile(minVals, (m,1))
+    normDataSet = normDataSet/tile(ranges, (m,1))
+    return normDataSet, ranges, minVals
 
 
 # 测试
 def datingClassTest():
-	hoRatio = 0.10
-	datingDataMat, datingLabels = file2matrix('datingTestSet2.txt')
-	normMat, ranges, minVals = autoNorm(datingDataMat)
-	m = normMat.shape[0]
-	numTestVecs = int(m*hoRatio)
-	errorCount = 0.0
-	for i in range(numTestVecs):
-		classifierResult = classify0(normMat[i,:], normMat[numTestVecs:m,:], datingLabels[numTestVecs:m], 3)
-		print 'the classifier came back with: %d, the real answer is: %d' % (classifierResult, datingLabels[i])
-		if (classifierResult != datingLabels[i]): errorCount += 1.0
-	print 'the total error rate is: %f' % (errorCount/float(numTestVecs))
+    hoRatio = 0.10
+    datingDataMat, datingLabels = file2matrix('datingTestSet2.txt')
+    normMat, ranges, minVals = autoNorm(datingDataMat)
+    m = normMat.shape[0]
+    numTestVecs = int(m*hoRatio)
+    errorCount = 0.0
+    for i in range(numTestVecs):
+        classifierResult = classify0(normMat[i,:], normMat[numTestVecs:m,:], datingLabels[numTestVecs:m], 3)
+        print 'the classifier came back with: %d, the real answer is: %d' % (classifierResult, datingLabels[i])
+        if (classifierResult != datingLabels[i]): errorCount += 1.0
+    print 'the total error rate is: %f' % (errorCount/float(numTestVecs))
 
 
 # 将一个图片文件转换成一行数据
 def img2vector(filename):
-	returnVect = zeros((1,1024))
-	fr = open(filename)
-	for i in range(32):
-		lineStr = fr.readline()
-		for j in range(32):
-			returnVect[0, 32*i+j] = int(lineStr[j])
-	return returnVect
+    returnVect = zeros((1,1024))
+    fr = open(filename)
+    for i in range(32):
+        lineStr = fr.readline()
+        for j in range(32):
+            returnVect[0, 32*i+j] = int(lineStr[j])
+    return returnVect
+
+
+# 手写识别测试
+def handwritingClassTest():
+    hwLabels = []
+    trainingFileList = listdir('trainingDigits')
+    m = len(trainingFileList)
+    trainingMat = zeros((m,1024))
+    for i in range(m):
+        fileNameStr = trainingFileList[i]
+        fileStr = fileNameStr.split('.')[0]
+        classNumStr = int(fileNameStr.split('_')[0])
+        hwLabels.append(classNumStr)
+        trainingMat[i,:] = img2vector('trainingDigits/%s' % fileNameStr)
+    testFileList = listdir('testDigits')
+    errorCount = 0.0
+    mTest = len(testFileList)
+    for i in range(mTest):
+        fileNameStr = testFileList[i]
+        fileStr = fileNameStr.split('.')[0]
+        classNumStr = int(fileStr.split('_')[0])
+        vectorUnderTest = img2vector('testDigits/%s' % fileNameStr)
+        classifierResult = classify0(vectorUnderTest, trainingMat, hwLabels, 3)
+        print 'the classifier came back with: %d, the real answer is: %d' % (classifierResult, classNumStr)
+        if (classifierResult != classNumStr): errorCount += 1.0
+    print '\nthe total number of errors is: %d' %errorCount
+    print '\nthe total error rate is: %f' % (errorCount/float(mTest))
