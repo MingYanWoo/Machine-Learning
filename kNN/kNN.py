3fdcfc32f15d0b729752a884d12f5f01e27b79ea
MingYanWoo <woomingyan@gmail.com>
2018-04-30 04:02:06 +0800

    tab

--- kNN/kNN.py
+++ kNN/kNN.py
@@ -2,42 +2,46 @@
 from numpy import *
 import operator
 
+
 # 创建数据集
 def createdataset():
-	group = array([[1.0, 1.1], [1.0, 1.0], [0, 0], [0, 0.1]])
-	labels = ['A', 'A', 'B', 'B']
-	return group, labels
+    group = array([[1.0, 1.1], [1.0, 1.0], [0, 0], [0, 0.1]])
+    labels = ['A', 'A', 'B', 'B']
+    return group, labels
+
 
 # kNN 算法
 def classify0(inX, dataSet, labels, k):
-	dataSetSize = dataSet.shape[0]
-	diffMat = tile(inX, (dataSetSize, 1)) - dataSet
-	sqDiffMat = diffMat**2
-	sqDistances = sqDiffMat.sum(axis=1)
-	distances = sqDistances**0.5
-	sortedDistIndicies = distances.argsort()
-	classCount = {}
-	for i in range(k):
-		voteIlabel = labels[sortedDistIndicies[i]]
-		classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1
-	sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse=True)
-	return sortedClassCount[0][0]
+    dataSetSize = dataSet.shape[0]
+    diffMat = tile(inX, (dataSetSize, 1)) - dataSet
+    sqDiffMat = diffMat**2
+    sqDistances = sqDiffMat.sum(axis=1)
+    distances = sqDistances**0.5
+    sortedDistIndicies = distances.argsort()
+    classCount = {}
+    for i in range(k):
+        voteIlabel = labels[sortedDistIndicies[i]]
+        classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1
+    sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse=True)
+    return sortedClassCount[0][0]
+
 
 # 创建数据集
 def file2matrix(filename):
-	fr = open(filename)
-	arrayOLines = fr.readlines()
-	numberOfLines = len(arrayOLines)
-	returnMat = zeros((numberOfLines, 3))
-	classLabelVector = []
-	index = 0
-	for line in arrayOLines:
-		line = line.strip()
-		listFromLine = line.split('\t')
-		returnMat[index,:] = listFromLine[0:3]
-		classLabelVector.append(int(listFromLine[-1]))
-		index += 1
-	return returnMat, classLabelVector
+    fr = open(filename)
+    arrayOLines = fr.readlines()
+    numberOfLines = len(arrayOLines)
+    returnMat = zeros((numberOfLines, 3))
+    classLabelVector = []
+    index = 0
+    for line in arrayOLines:
+        line = line.strip()
+        listFromLine = line.split('\t')
+        returnMat[index,:] = listFromLine[0:3]
+        classLabelVector.append(int(listFromLine[-1]))
+        index += 1
+    return returnMat, classLabelVector
+
 
 # 归一化特征值
 def autoNorm(dataSet):
@@ -49,6 +53,7 @@ def autoNorm(dataSet):
 	normDataSet = dataSet - tile(minVals, (m,1))
 	normDataSet = normDataSet/tile(ranges, (m,1))
 	return normDataSet, ranges, minVals
+
 
 # 测试
 def datingClassTest():
@@ -63,6 +68,7 @@ def datingClassTest():
 		print 'the classifier came back with: %d, the real answer is: %d' % (classifierResult, datingLabels[i])
 		if (classifierResult != datingLabels[i]): errorCount += 1.0
 	print 'the total error rate is: %f' % (errorCount/float(numTestVecs))
+
 
 # 将一个图片文件转换成一行数据
 def img2vector(filename):
