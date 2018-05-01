c2ce41ed22e85a811895435796968e3fc01f3c14
MingYanWoo <woomingyan@gmail.com>
2018-05-01 17:24:02 +0800

    first commit

---
+++ Decision-Tree/trees.py
@@ -0,0 +1,65 @@
+# -*- coding:utf-8 -*-
+from math import log
+
+
+# 创建数据集
+def createDataSet():
+    dataSet = [[1, 1, 'yes'],
+               [1, 1, 'yes'],
+               [1, 0, 'no'],
+               [0, 1, 'no'],
+               [0, 1, 'no']]
+    labels = ['no surfacing', 'flippers']
+    return dataSet, labels
+
+
+# 计算数据集的香农熵
+def calcShannonEnt(dataSet):
+    numEntries = len(dataSet)
+    labelCounts = {}
+    for featVec in dataSet:
+        currentLabel = featVec[-1]
+        if currentLabel not in labelCounts.keys():
+            labelCounts[currentLabel] = 0
+        labelCounts[currentLabel] += 1
+    shannonEnt = 0.0
+    for key in labelCounts:
+        prob = float(labelCounts[key])/numEntries
+        shannonEnt -= prob * log(prob, 2)
+    return shannonEnt
+
+
+# 按给定特征划分数据集
+def splitDataSet(dataSet, axis, value):
+    retDataSet = []
+    for featVec in dataSet:
+        if featVec[axis] == value:
+            reduceFeatVec = featVec[:axis]
+            reduceFeatVec.extend(featVec[axis+1:])
+            retDataSet.append(reduceFeatVec)
+    return retDataSet
+
+
+# 选择最好的数据集划分方式(信息增益最高)
+def chooseBestFeatureToSplit(dataSet):
+    numFeatures = len(dataSet[0]) - 1
+    baseEntropy = calcShannonEnt(dataSet)
+    bestInfoGain = 0.0
+    bestFeature = -1
+    for i in range(numFeatures):
+        featList = [example[i] for example in dataSet]
+        uniqueVals = set(featList)                                      # 创建唯一的分类标签列表
+        newEntropy = 0.0
+        for value in uniqueVals:                                        # 计算每种划分方式的信息熵
+            subDataSet = splitDataSet(dataSet, i, value)
+            prob = len(subDataSet)/float(len(dataSet))
+            newEntropy += prob * calcShannonEnt(subDataSet)
+        infoGain = baseEntropy - newEntropy                             # 信息增益
+        if (infoGain > bestInfoGain):                                   # 计算最好的信息增益
+            bestInfoGain = infoGain
+            bestFeature = i
+    return bestFeature
+
+
+
+
